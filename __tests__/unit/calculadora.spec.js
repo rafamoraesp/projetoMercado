bcc1230bb7f9afa10e9ad912d256077189d50cd6
Rafael Moraes <rafamoraesp@gmail.com>
2022-10-13 21:49:01 -0300

    Teste de quatro operacoes matematicas.

--- __tests__/unit/calculadora.spec.js
+++ __tests__/unit/calculadora.spec.js
@@ -4,21 +4,54 @@ const calculadora = require("../../src/calculadora.js");
 
 //Funções de testes de unidade
 test("somar 5 + 7", () => {
-    // 1 - Configura
+    // 1 - Configura / Arrange
     // 1.1 Dados de Entrada
     const num1 = 5;
     const num2 = 7;
 
-    // 1.2 Resultado Esperado
+    // 1.2 Resultado Esperado / saída
     const resultadoEsperado = 12;
 
-    // 2 - Executa
+    // 2 - Executa / Act
     // criando um objeto para receber a funcao da calculadora
     const somarDoisNumeros = calculadora.somarDoisNumeros;
     // executando a funcao DoisNumeros e guardando o resultado
     const resultadoAtual = somarDoisNumeros(num1, num2);
     
-    // 3 - Valida
+    // 3 - Valida / Assert
     expect(resultadoAtual).toBe(resultadoEsperado);
 
+})
+
+test("subtrair 15 - 7", () => {
+    const num1 = 15;
+    const num2 = 7;
+    const resultadoEsperado = 8;
+
+    const subtrairDoisNumeros = calculadora.subtrairDoisNumeros;
+    const resultadoAtual = subtrairDoisNumeros(num1, num2);
+
+    expect(resultadoAtual).toBe(resultadoEsperado);
+})
+
+test("multiplicar 3 * 7", () => {
+    const num1 = 3;
+    const num2 = 7;
+    const resultadoEsperado = 21;
+
+    const multiplicarDoisNumeros = calculadora.multiplicarDoisNumeros;
+    const resultadoAtual = multiplicarDoisNumeros(num1, num2);
+
+    expect(resultadoAtual).toBe(resultadoEsperado);
+})
+
+test("Dividir 27 / 9", () => {
+    const num1 = 27;
+    const num2 = 9;
+    const resultadoEsperado = 3;
+
+    const dividirDoisNumeros = calculadora.dividirDoisNumeros;
+    const resultadoAtual = dividirDoisNumeros(num1, num2);
+
+    expect(resultadoAtual).toBe(resultadoEsperado);
 })
