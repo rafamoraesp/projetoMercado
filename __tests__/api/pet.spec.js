cf1294760374e524140470eccd4d2392d0e35b81
Rafael Moraes <rafamoraesp@gmail.com>
2022-10-27 22:09:25 -0300

    Post, Get, Put, Delete

--- __tests__/api/pet.spec.js
+++ __tests__/api/pet.spec.js
@@ -28,6 +28,43 @@ describe("PetStore Swagger - Pet", () => {
                 assert.equal(response.body.id, petId);  // Se é o id esperado do animal
                 assert.equal(response.body.name, "Ice"); // Se é o nome esperado
                 assert.equal(response.body.status, "available"); // Se está com o status esperado
-            })
+            });
     });
+
+    it("Get Pet", () => {
+        return request                                 // chamada para a requisição
+            .get("/pet/" + petId)                      // consultar o animal pelo id
+            .then((response) => {                      // tratar a resposta / retorno
+                assert.equal(response.statusCode, 200);
+                assert.equal(response.body.id, petId);
+                assert.equal(response.body.name, "Ice");
+                assert.equal(response.body.status, "available");
+            });
+    });
+
+    it("Put Pet", () => {
+        const jsonFile = require("../../vendors/json/pet2.json");
+        return request                                  // realizar a requisição
+            .put("/pet")                                //alterar o animal - aponta para o endpoint
+            .send(jsonFile)                             // json com a alteração
+            .then((response) => {                       // receber e validar a resposta
+                assert.equal(response.statusCode, 200);
+                assert.equal(response.body.id, petId);
+                assert.equal(response.body.name, "Ice");
+                assert.equal(response.body.tags[1].id, 4);
+                assert.equal(response.body.tags[1].name, "castrated");
+                assert.equal(response.body.status, "sold");
+
+            });
+    });
+
+    it("Delete Pet", () => {
+        return request
+            .delete("/pet/" + petId)
+            .then((response) => {
+                assert.equal(response.statusCode, 200)
+            });
+                
+    });
+
 });
